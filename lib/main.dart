ab62890261b6e38ad34972c0bcdf536626f8f1aa
eloge kemgang <elogekemgang@gmail.com>
2025-08-07 14:32:28 +0100

    Troisieme commit : ajout de (multiDesxEnabled = true) dans le defaultConfig de android/app/build.gradle.kts pour que le gradle gere bient tout les fichier de l'app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
-import 'package:untitled2/firebase_options.dart';
-
 
 void main() async {
 
